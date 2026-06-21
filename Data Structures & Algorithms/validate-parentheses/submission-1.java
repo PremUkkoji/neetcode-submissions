990b0a31937c05968985dd51e93b3adc3e9f310f
Prem Ukkoji <premukkoji@gmail.com>
2026-06-21 12:25:44 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(char ch: s.toCharArray()) {
+            if(ch == '{' || ch == '[' || ch == '(') {
+                st.push(ch);
+            } else {
+                if(ch == '}' && !st.isEmpty() && st.peek() == '{') {
+                    st.pop();
+                } else if(ch == ']' && !st.isEmpty() && st.peek() == '[') {
+                    st.pop();
+                } else if(ch == ')' && !st.isEmpty() && st.peek() == '(') {
+                    st.pop();
+                } else return false;
+            }
+        }
+
+        if(!st.isEmpty()) return false; else return true;
+    }
+}
