b4576f7ec9cb35b4fd7b7df196f168f4cd83f729
Prem Ukkoji <premukkoji@gmail.com>
2026-06-26 18:55:16 +0530

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int ans = 0;
+        for(int i=0;i<32;i++) {
+            ans += 1 & (n>>i);
+        }
+        return ans;
+    }
+}
